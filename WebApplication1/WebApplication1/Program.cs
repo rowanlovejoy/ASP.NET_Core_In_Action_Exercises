9aa66c1a4b1b3cd9cbc2b92ad8dacad10e710e40
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2025-03-21 21:20:09 +0000

    4.2.1 holding page app

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,20 +1,6 @@
-using Microsoft.AspNetCore.HttpLogging;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddHttpLogging(options => options.LoggingFields = HttpLoggingFields.RequestProperties);
-builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpLogging();
-}
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/person", () => new Person("Rowan", "Lovejoy"));
+app.UseWelcomePage();
 
 app.Run();
-
-public record Person(string FirstName, string LastName);
