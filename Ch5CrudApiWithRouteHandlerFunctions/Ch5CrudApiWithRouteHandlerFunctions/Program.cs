ca646811d143ac79556a818399002ad7a0df1f05
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2025-04-29 07:50:45 +0100

    Add endpoint demonstrating manual response definition

--- Ch5CrudApiWithRouteHandlerFunctions/Ch5CrudApiWithRouteHandlerFunctions/Program.cs
+++ Ch5CrudApiWithRouteHandlerFunctions/Ch5CrudApiWithRouteHandlerFunctions/Program.cs
@@ -1,34 +1,59 @@
+using System.Collections.Concurrent;
+using System.Net.Mime;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var fruitCollection = new ConcurrentDictionary<string, Fruit>();
+
 // Lambda handler
-app.MapGet("/fruit", () => Fruit.All);
+app.MapGet("/fruit", () => fruitCollection);
+
+//var getFruit = (string id) =>
+//{
+//    return Fruit.All[id];
+//};
 
 // The name of the parameter in the handler must match the name of the route param, otherwise an exception occurs when tring to match the request to a endpoint
-
-// Delegate handler
-var getFruit = (string id) =>
+app.MapGet("/fruit/{id}", (string id) =>
 {
-    return Fruit.All[id];
-};
-app.MapGet("/fruit/{id}", getFruit);
+    return fruitCollection.TryGetValue(id, out var fruit)
+        ? TypedResults.Ok(fruit)
+        // Can't use TypedResults.NotFound here or it complains the about the handler signature -- why?
+        : Results.NotFound();
+});
 
-// Static method handler
-app.MapPost("/fruit/{id}", Handlers.AddFruit);
+app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+{
+    return fruitCollection.TryAdd(id, fruit)
+        ? TypedResults.Created($"/fruit/{id}", fruit)
+        // Can't use TypedResults.NotFound here, either. Maybe TypedResults works only if you're returning a value (but we're returning an error object)
+        : Results.BadRequest(new { id = "A fruit with this ID already exists." });
+});
 
-// Instance method handler
-var handlers = new Handlers();
-app.MapPut("/fruit/{id}", handlers.ReplaceFruit);
+app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
+{
+    fruitCollection[id] = fruit;
+    return Results.NoContent();
+});
 
-// Local function handler
-app.MapDelete("/fruit/{id}", DeleteFruit);
+app.MapDelete("/fruit/{id}", (string id) =>
+{
+    return fruitCollection.TryRemove(id, out var fruit)
+        ? Results.NoContent()
+        : Results.BadRequest(new { id = $"No fruit exists with the {id}" });
+});
+
+// Endpoint with a manually defined response, i.e., created without TypedResults or Results
+app.MapGet("/teapot", (HttpResponse response) =>
+{
+    // Doesn't have a constant for 418 :(
+    response.StatusCode = 418;
+    response.ContentType = MediaTypeNames.Text.Plain;
+    return response.WriteAsync("I'm a teapot.");
+});
 
 app.Run();
-
-static void DeleteFruit(string id)
-{
-    Fruit.All.Remove(id);
-}
 
 internal record Fruit(string Name, int Stock)
 {
@@ -38,15 +63,15 @@ internal record Fruit(string Name, int Stock)
 // Thee fruit parameters will be retrieved by deserializing the JSON body of the request
 // This happens because there's no route parameter that could fulfil them; they're complex types
 // Only a single parameter can be bound to the request body
-internal class Handlers
-{
-    public void ReplaceFruit(string id, Fruit fruit)
-    {
-        Fruit.All[id] = fruit;
-    }
+//internal class Handlers
+//{
+//    public void ReplaceFruit(string id, Fruit fruit)
+//    {
+//        Fruit.All[id] = fruit;
+//    }
 
-    public static void AddFruit(string id, Fruit fruit)
-    {
-        Fruit.All.Add(id, fruit);
-    }
-}
+//    public static void AddFruit(string id, Fruit fruit)
+//    {
+//        Fruit.All.Add(id, fruit);
+//    }
+//}
