7e5b1730ae1fc6a5a8e92d7525ecc443fbc1d244
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2025-06-08 22:54:53 +0100

    Add app demonstrating different ways of reigstering endpoints

---
+++ Ch6EndpointRegistrationExampleApp/Ch6EndpointRegistrationExampleApp/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.MapGet("/test", () => "Hello World!");
+
+app.MapHealthChecks("/health");
+
+app.MapRazorPages();
+
+app.Run();
