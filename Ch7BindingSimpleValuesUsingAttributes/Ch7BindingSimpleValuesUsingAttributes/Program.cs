e1c3f55cab5101f2447da7b192e47ff5c4d9d7b7
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2025-06-14 12:34:20 +0100

    Add example demonstrating model binding with a custom "simple type"

--- Ch7BindingSimpleValuesUsingAttributes/Ch7BindingSimpleValuesUsingAttributes/Program.cs
+++ Ch7BindingSimpleValuesUsingAttributes/Ch7BindingSimpleValuesUsingAttributes/Program.cs
@@ -1,16 +1,69 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // By default, ASP.NET attempts to bind first from route parameters and second from query parameters
-app.MapGet("/products/{id}/paged", (
-    // Can use From* attributes to override default binding sources, explicitly stating where an endpoint's parameter(s) should be sourced from
+app.MapGet("/products/{id:int}/paged", (
+    // Can use [From*] attributes to override default binding sources, explicitly defining where an endpoint's parameter(s) should be sourced from
+    // These three attributes are the only parameter binding attributes that operator on "simple values", e.g., int, double
+    // The book gives int and double as examples of simple types (see below)
     [FromRoute] int id,
     [FromQuery] int page,
-    [FromHeader(Name = "PageSize")] int pageSize) =>
+    // Route and query parameters can be bound automatically based on convention -- try first from route and second from query -- but the [FromHeader] attribute is required to bind to a request header
+    [FromHeader(Name = "PageSize")] int pageSize,
+    [FromHeader(Name = "MyString")] string myString, [FromHeader(Name = "MyBool")] bool myBool) =>
 {
-    return $"Received ID {id}, page {page}, and pageSize {pageSize}";
+    return new Dictionary<string, object>
+    {
+        { "id", id },
+        { "page", page },
+        { "PageSize", pageSize },
+        { "MyString", myString },
+        { "MyBool", myBool }
+    };
+});
+
+app.MapGet("/products/{id}", (ProductId id) =>
+{
+    return $"The product ID is {id}";
 });
 
 app.Run();
+
+// A simple type for the purposes of ASP.NET core model binding (are Razer Pages and MVC different?) is a type that implements the method "public static bool TryParse(string value, out T result)" or an overload that additionally takes an IFormatProvider as its second argument
+// The example below explicitly implements the IParsable interface that includes a suitable TryParse method (and also Parse), but the interface isn't required; the type has only to have method with the right signature. The ASP.NET Core calls this method during model binding to create argument for the endpoint handler.
+// So, any type could, regardless of complexity, could be a simple type as long as it implements TryFormat in a compatible way
+// The example below demonstrates this with a strongly typed ID type: strings matching "p<number>", e.g., "p123" will be converted into ProductId instances with the number segment of the input string as the value of the Id property, e.g., "p123" will become ProductId { Id = 123 }
+internal readonly record struct ProductId(int Id) : IParsable<ProductId>
+{
+    public static ProductId Parse(string s, IFormatProvider? provider)
+    {
+        if (s is ['p', .. var numberSegment] && int.TryParse(numberSegment, out var idNumber))
+        {
+            return new ProductId(idNumber);
+        }
+
+        throw new FormatException(s);
+    }
+
+    // If TryParse returns true during model binding, the resulting value will be passed to the matching parameter of the endpoint handler; if it returns false, an exception is thrown resulting in 400 Bad Request response
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out ProductId result)
+    {
+        if (s is ['p', .. var numberSegment] && int.TryParse(numberSegment, out var idNumber))
+        {
+            result = new ProductId(idNumber);
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return Id.ToString();
+    }
+}
+
