8d9b3dbddf9e1cc27197e2e9c398e562821c6a7d
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2025-11-10 08:48:18 +0000

    Improve comments, fix incorrect binding in UserIdModel

--- Ch7DataAnnotationsAttributesForValidation/Ch7DataAnnotationsAttributesForValidation/Program.cs
+++ Ch7DataAnnotationsAttributesForValidation/Ch7DataAnnotationsAttributesForValidation/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapPost("/users", (UserModel user) => user.ToString())
-   // ASP.NET doesn't contain a built-in validator for Minimal APIs. Microsoft's expects developers to source a third-party package or create their own.
+   // ASP.NET doesn't contain a built-in validator for Minimal APIs (*) (it does for Razor and MCV projects). DataAnnotations metadata to generate and return validation results. Microsoft's expects developers to source a third-party package or create their own.
    // A member of ASP.NET team published the MinimalApi.Extensions package (apparently unofficially), which contains the WithParameterValidation() extension method.
    // This method adds an endpoint filter that implements validation referencing the DataAnnotations attributes applied to the members of the endpoint handler parameters.
    .WithParameterValidation();
@@ -13,18 +13,24 @@ app.MapPost("/users", (UserModel user) => user.ToString())
 app.MapPost("/users/create", (CreateUserModel user) => user.ToString())
    .WithParameterValidation();
 
-// Simple types declared directly in the endpoint handler signature cannot use DataAnnotations attributes for validation; the validator (*) looks specifically at the properties of types and whether they have DataAnnotions attributes, not the containing type instance itself.
-// One solution to this limitation is to use the [AsParameters] attribute and wrap the simple type to validated in a container; it will then be a property of its containing type and DataAnnotations attributes attached to it will be considered during validation.
+// Simple types declared directly in the endpoint handler signature cannot use DataAnnotations attributes for validation; the validator (**) looks specifically at the properties of types and whether they have DataAnnotations attributes; applying attributes directly to the parameter, as in the first example below, will therefore not work. Technically, int is type that has properties, one of which contains the integral value, but we can't access it to apply DataAnnotations attributes.
+// One solution to this limitation is to use the [AsParameters] attribute and wrap the simple type parameter to be validated in a container type; the wrapped parameter will then be a property of its containing type, DataAnnotations attributes can therefore be attached to it, and they will be considered during validation.
+// app.MapPost("/users/{id}", ([Range(1, 100) int id) => userId.ToString())
+//   .WithParameterValidation();
 app.MapPost("/users/{id}", ([AsParameters] UserIdModel userId) => userId.ToString())
    .WithParameterValidation();
 
 app.Run();
 
-// * By default, the framework doesn't include a validator, code that reads the metadata added by the DataAnnoations attributes and uses it to validate the contents of properties. In this project, I'm using the MinimalApi.Extensions package and its WithParameterValidation() for this purpose. Would another validator implementation not have this limitation and thus be able to inspect attributes applied directly to endpoint parameters and validate the contents of those parameters?
+// * To be specific, Minimal APIs doesn't include an API for automatically performing validation on endpoint parameters using the associated DataAnnotations metadata. The static DataAnnotations.Validator class appears to be a validator. Its validation methods operate on the metadata applied via DataAnnotations attributes and populates an enumerable with validation results should any of the fields not be in the specified form.
+// This class appears to have existed for some time, apparently predating ASP.NET Core Maybe this class is what powers validation for Razor and MCV projects.
+// Presumably, I could create a custom filter that implements validation much the same as MinimalApi.Extensions by calling on the methods of DataAnnotations.Validate.
 
-// Attributes in the DataAnnotations namespace apply metadata to properties of a type than subsequently be used to validate the value it contains
+// ** Minimal APIs doesn't include a built-in solution that automatically reads the metadata added by the DataAnnotations attributes and uses it to validate the contents of properties. In this project, I'm using the MinimalApi.Extensions package and its WithParameterValidation() for this purpose; this method creates an endpoint filter that reads the metadata and performs validation, using a library called MiniValidator, which itself seems to rely on the underlying DataAnnotations.Validator. Is it DataAnnotations.Validator that defines that only annotations applied to properties will be examined, and not those applied to containing type itself? Would another validator implementation not have this limitation and thus be able to inspect attributes applied directly to endpoint parameters and validate the contents of those parameters?
+
+// Attributes in the DataAnnotations namespace apply metadata to properties of a type that can subsequently be used to validate the value it contains.
 // These attributes can specify whether a value is required, it's minimum and maximum length, its range, and whether it is formatted correctly (e.g., whether it's a correctly formed email address or phone number), and more.
-// By themselves, these attributes only provide metadata, and this metadata must be manually read and used in a validation process.
+// By themselves, these attributes provide only metadata, and this metadata must be manually read and used in a validation process by some other code.
 internal record UserModel
 {
     [Required]
@@ -45,9 +51,9 @@ internal record UserModel
     public string? PhoneNumber { get; set; }
 }
 
-// DataAnnotations attributes are primarily useful for validating the contents of individual properties discretely (for one exception, see the [Compare] attribute).
-// For more complex validation that can access and compare one or more properties simultaneously and perform complex processing (this may also be achievable with a custom attribute), types can implement IValidatableObject, which includes the Validate method.
-// During validation, this method will be executed if each property first satisfies its individual validation attributes**, e.g., a value is present if [Required] and is a correctly formatted [EmailAddress]. If any property fails its individual validation, IValidatableObject.Validate will never be called.
+// DataAnnotations attributes are primarily useful for validating the contents of individual properties discretely (with the possible exception of the [Compare] attribute).
+// For more complex validation that can access and compare one or more properties simultaneously and perform complex processing (complex customer may also be achievable with a custom attribute), types can implement IValidatableObject, which includes the Validate method.
+// During validation, this method will be executed if each property first satisfies its individual validation attributes (***), e.g., a value is present if [Required] and is a correctly formatted [EmailAddress]. If any property fails its individual validation, IValidatableObject.Validate will never be called.
 internal record CreateUserModel : IValidatableObject
 {
     [EmailAddress]
@@ -66,11 +72,11 @@ internal record CreateUserModel : IValidatableObject
     }
 }
 
-// ** Since the validation implementation is coming from this third-party package, could another validator, e.g., from a different package, decide to call IValidatableObject.Validate earlier in the process? The book describes this order of validation -- first individual properties, then Validate() -- before describing that that validator is external code that must be brought in. Is it a hard and fast rule that this order must be applied, or could a difgerent validator do it differently?
+// *** As per **, could another validator, e.g., from a different package, decide to call IValidatableObject.Validate earlier in the process? The book describes this order of validation -- first individual properties, then Validate() -- before describing that that validator is external code that must be brought in. Is it a hard and fast rule that this order must be applied, or could a another validator do it differently? Or is it that all external validators ultimately depend on DataAnnotations.Validate and it is this class that determines this order of operations, and that is why the book described this behaviour before introducing the need for a validator?
 
 internal record UserIdModel
 {
-    [FromQuery(Name = "id")]
+    [FromRoute(Name = "id")]
     [Range(1, 100)]
     public required int Id { get; init; }
 }
