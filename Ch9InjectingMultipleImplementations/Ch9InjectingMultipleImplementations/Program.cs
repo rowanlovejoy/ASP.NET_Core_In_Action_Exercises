ed08544e59d4a3672143de32a278db3498130fc2
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2026-01-09 22:25:54 +0000

    Add example demonstrating injecting multiple implementations of the same service as a single parameter

---
+++ Ch9InjectingMultipleImplementations/Ch9InjectingMultipleImplementations/Program.cs
@@ -0,0 +1,99 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// If there are multiple implememtations of a service, it's possible to each inject an instance of each in a single parameter.
+// Each implenetation of the service -- defined by some kind of base type, class or interface, in this case IMessageSender -- must be registered with the DI container, as individual services would be.
+builder.Services.AddScoped<IMessageSender, EmailSender>();
+builder.Services.AddScoped<IMessageSender, SmsSender>();
+builder.Services.AddScoped<IMessageSender, DiscordSender>();
+
+// To have multiple implementations of a service, multiple implementations of the same interface or base class are required.
+// In this example, the service is defined by non-abstract a class; this base includes an implementation itself (hence its appearence in both the service and implememtation generic parameters), and I'm also registering its two derived classes as is implementations.
+builder.Services.AddScoped<ColourPrinter, ColourPrinter>();
+builder.Services.AddScoped<ColourPrinter, InkjetPrinter>();
+builder.Services.AddScoped<ColourPrinter, LaserPrinter>();
+
+var app = builder.Build();
+
+app.MapGet("/register/{username}", RegisterUser);
+app.MapGet("/print/{message}", PrintMessage);
+
+app.Run();
+
+// To inject an instance of each service implementation in an endpoint handler, the handler must accept a parameter of IEnumerable<T>, where T is the type defining the service.
+// The DI container will inject an argument to this parameter that is an array of T (T[]), containing one item for each registered implementati of the service, in the same order in which they were registered with the DI container, i.e., the first implementation registered will have its instance in the first slot of the array.
+string RegisterUser(string username, IEnumerable<IMessageSender> senders)
+{
+    // Actions can be performed with the injected implementations by simply looping over them, e.g., with a foreach loop.
+    foreach (var sender in senders)
+    {
+        sender.SendMessage($"Welcome {username}");
+    }
+
+    return $"Welcome message sent to {username}";
+}
+
+// This handler will receive an array containing the three implementations of the ColourPrinter service: ColourPrinter itself, as a non-abstract class containing an implementation; and its two derived classes, InkjetPrinter and LaserPrinter
+string PrintMessage(string message, IEnumerable<ColourPrinter> printers)
+{
+    foreach (var printer in printers)
+    {
+        printer.Print(message);
+    }
+
+    return $"Printed message \"{message}\" using all printers";
+}
+
+internal interface IMessageSender
+{
+    void SendMessage(string message);
+}
+
+public class EmailSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Sending message via email");
+    }
+}
+
+public class SmsSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Sending message via SMS");
+    }
+}
+
+public class DiscordSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Sending message via Discord DM");
+    }
+}
+
+public class ColourPrinter
+{
+    public virtual void Print(string message)
+    {
+        Console.WriteLine("Printing in colour...");
+    }
+}
+
+public class InkjetPrinter : ColourPrinter
+{
+    public override void Print(string message)
+    {
+        base.Print(message);
+        Console.WriteLine($"Printing message using inkjet: {message}");
+    }
+}
+
+public class LaserPrinter : ColourPrinter
+{
+    public override void Print(string message)
+    {
+        base.Print(message);
+        Console.WriteLine($"Printing message using laser: {message}");
+    }
+}
