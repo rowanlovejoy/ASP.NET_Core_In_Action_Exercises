9e5111ccc82f70a782bbaff6cd4b320a09d293c0
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2026-01-10 17:12:36 +0000

    Add examples demonstrating conditional registration and replacement of implementations

--- Ch9InjectingMultipleImplementations/Ch9InjectingMultipleImplementations/Program.cs
+++ Ch9InjectingMultipleImplementations/Ch9InjectingMultipleImplementations/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // If there are multiple implementations of a service, it's possible to each inject an instance of each in a single parameter.
@@ -5,6 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IMessageSender, EmailSender>();
 builder.Services.AddScoped<IMessageSender, SmsSender>();
 builder.Services.AddScoped<IMessageSender, DiscordSender>();
+// The TryAdd* extension methods can be used to conditionally registered a service implementation if an implementation of the same service hasn't already been registered.
+// In this example, the RedditSender implementation of the IMessageSender service won't be registered because at least one other implementation has been already (three in this case: EmailSender, SmsSender, and DiscordSender).
+// If the previous three implementations registered were not registered, RedditSender would be registered.
+builder.Services.TryAddSingleton<IMessageSender, RedditSender>();
+
+// It's possible to explicitly replace a previously registered implementation using the Replace extension method.
+// If another implementation of the IMessageSender service has been registered, this Replace call will replace it with the RedditSender implementation.
+// If no previous implementation has been registered, this call will simply register the RedditSender implementation with the specified lifetime, in this case scoped.
+// The book says that to replace a previously registered implementation, the Replace call must specify the same lifetime as the implementation to be replaced; e.g., a scoped implementation can be replaced only if with another scoped implementation. However, this doesn't appear to be case, at least in ASP.NET 10. Here, I'm replacing a scoped implementation with a singleton, and the replacement is occurring as expected. Perhaps this was a restriction present in ASP.NET 7, the subject of the book, but that has since been removed.
+builder.Services.Replace(new ServiceDescriptor(typeof(IMessageSender), typeof(DiscordSender), ServiceLifetime.Singleton));
 
 // To have multiple implementations of a service, multiple implementations of the same interface or base class are required.
 // In this example, the service is defined by non-abstract a class; this base includes an implementation itself (hence its appearance in both the service and implementation generic parameters), and I'm also registering its two derived classes as is implementations.
@@ -35,6 +47,7 @@ string RegisterUser(string username, IEnumerable<IMessageSender> senders)
 
 // If multiple implementations of a service are registered, but an injection site, such as an endpoint handler, requires only a single instance, the last implementation registered will be the instance in injected.
 // This endpoint handler requires a single instance of the IMessageSender service; as DiscordSender was the last implementation of the IMessageSender service to be registered, an instance of this implementation will be injected.
+// This behaviour can be useful for overriding an implementation added by the framework or a third-party library, for example; just ensure your implementation is registered last, and it will be injected wherever a single implementation of that service is required.
 string RegisterUser2(string username, IMessageSender sender)
 {
     sender.SendMessage(username);
@@ -79,6 +92,14 @@ public class DiscordSender : IMessageSender
     public void SendMessage(string message)
     {
         Console.WriteLine("Sending message via Discord DM");
+    }
+}
+
+public class RedditSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Sending message via Reddit DM");
     }
 }
 
